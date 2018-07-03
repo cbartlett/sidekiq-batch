d3d206217f04268f6c6ec4f4304e39e85e9bbf9b
Colin Bartlett <colin@colinabartlett.com>
2018-07-03 15:59:32 -0400

    Require ActiveSupport for tests

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gemspec
 
 group :test do
+  gem "activesupport"
   gem "simplecov"
   gem "codeclimate-test-reporter", "~> 1.0.0"
   gem 'pry-byebug'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'fakeredis/rspec'
 require 'sidekiq/batch'
 require 'pry-byebug'
+require 'active_support/all'
 
 redis_opts = { url: "redis://127.0.0.1:6379/1", id: nil }
 redis_opts[:driver] = Redis::Connection::Memory if defined?(Redis::Connection::Memory)
