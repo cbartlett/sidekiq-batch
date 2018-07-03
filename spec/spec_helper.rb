91ba4f13aca34d6c64a6c932c0c85aa93ef19b0c
Colin Bartlett <colin@colinabartlett.com>
2018-07-03 15:58:06 -0400

    Fix Redis version compatibility issue
    https://github.com/mperham/sidekiq/issues/3518

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'fakeredis/rspec'
 require 'sidekiq/batch'
 require 'pry-byebug'
 
-redis_opts = { url: "redis://127.0.0.1:6379/1" }
+redis_opts = { url: "redis://127.0.0.1:6379/1", id: nil }
 redis_opts[:driver] = Redis::Connection::Memory if defined?(Redis::Connection::Memory)
 
 Sidekiq.configure_client do |config|
